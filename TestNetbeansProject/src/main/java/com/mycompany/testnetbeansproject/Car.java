5f3988c59d9ced4e003a6a52b7200484ca5c3e37
Plamen <plammm@abv.bg>
2019-06-19 14:00:27 +0300

    My commit

---
+++ TestNetbeansProject/src/main/java/com/mycompany/testnetbeansproject/Car.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testnetbeansproject;
+
+/**
+ *
+ * @author PI
+ */
+public class Car {
+    private int fuel;
+}
